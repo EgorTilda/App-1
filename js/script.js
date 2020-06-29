efec5862a2cb3bd4f2391e156a9d3a1183932401
Egor <egortilda@gmail.com>
2020-06-29 21:22:12 +0300

    objects + script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+//const arr = [1, 2, 3, 6, 8];
+//arr.pop();
+//arr.push(10);
+//console.log(arr);
+//arr[99] = 0;
+//console.log(arr.length);
+//console.log(arr);
+
+//arr.forEach(function(item, i, arr) {
+  //console.log(`${i}: ${item} внутри массива ${arr}`);
+//});
+
+//const str = prompt('', '');
+//const products = str.split(', ');
+//products.sort();
+//console.log(products.join('; '));
